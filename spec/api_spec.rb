9c723e74195977af9aebe1991ab7686e10d3e363
Kohei MATSUSHITA <ma2shita+git@ma2shita.jp>
2014-11-28 19:58:42 +0900

    Tweak to spec indent

--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -7,21 +7,19 @@ describe App::API do
     App::API
   end
 
-  describe App::API do
-    describe "GET /api/hello" do
-      before do
-        Timecop.freeze(2014, 12, 20, 15, 26, 37)
-      end
-      after do
-        Timecop.return
-      end
+  describe "GET /api/hello" do
+    before do
+      Timecop.freeze(2014, 12, 20, 15, 26, 37)
+    end
+    after do
+      Timecop.return
+    end
 
-      it "return Hello w/ now time" do
-        get "/api/hello"
-        expect(last_response.status).to eq 200
-        b = JSON.parse last_response.body
-        expect(b["hello"]).to eq "Hello! Today is 2014-12-20 15:26:37 +0900"
-      end
+    it "return Hello w/ now time" do
+      get "/api/hello"
+      expect(last_response.status).to eq 200
+      b = JSON.parse last_response.body
+      expect(b["hello"]).to eq "Hello! Today is 2014-12-20 15:26:37 +0900"
     end
   end
 end
--- spec/web_spec.rb
+++ spec/web_spec.rb
@@ -7,18 +7,16 @@ describe App::Web do
     App::Web
   end
 
-  describe App::Web do
-    describe "GET /" do
-      it "return Index" do
-        get "/"
-        expect(last_response.status).to eq 200
-        b = last_response.body
-        expect(b).to have_tag(:title, :text => "Sinatra Bootstrap")
-        expect(b).to have_tag(:h1, :text => /^This is .+ Bootstrap$/)
-        expect(b).to have_tag(:p) {
-          with_tag(:a, :with => {:href => "/api/hello" })
-        }
-      end
+  describe "GET /" do
+    it "return Index" do
+      get "/"
+      expect(last_response.status).to eq 200
+      b = last_response.body
+      expect(b).to have_tag(:title, :text => "Sinatra Bootstrap")
+      expect(b).to have_tag(:h1, :text => /^This is .+ Bootstrap$/)
+      expect(b).to have_tag(:p) {
+        with_tag(:a, :with => {:href => "/api/hello" })
+      }
     end
   end
 end
